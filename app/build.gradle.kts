b32060f3ba0581d8d663c23dbc2460490e7c366d
bakituncher <tuncerbaki627@gmail.com>
2025-08-02 15:05:39 +0300

    1.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.codenzi.snapnote"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.2.6"
+        versionCode = 27
+        versionName = "1.2.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
